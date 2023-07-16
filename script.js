0401a4cf4eb4fe5f2826acf028ef62119c1c9c2c
Nitish sharma <sharmanitish183@gmail.com>
2023-07-16 17:39:34 +0530

    active navlist while scrolling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+const sections = document.querySelectorAll('section');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -22,4 +23,18 @@ menu_item.forEach((item) => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
+});
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        let sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 65) {
+            current = section.getAttribute('id');
+        }
+    });
+    menu_item.forEach(li => {
+        li.classList.remove('active');
+        document.querySelector('.header .nav-bar .nav-list ul li a[href*= ' + current + ']').classList.add('active');
+    });
 });
